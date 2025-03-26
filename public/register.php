aff346dc3b6e37ba6d4e659e5495d0eee9f3eaab
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-03-26 15:40:38 +0100

    feat: added username input

--- public/register.php
+++ public/register.php
@@ -26,6 +26,8 @@
     <div class="login-register-container">
         <h1>Register New Account</h1>
         <form action="" method="post">
+            <label for="username">Username</label>
+            <input type="text" name="username" placeholder="Enter your username">
             <label for="email">Email Address</label>
             <input type="email" name="email" placeholder="Enter your email" />
             <label for="pass">Password</label>
